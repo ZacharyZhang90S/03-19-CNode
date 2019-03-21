4d2533ae1427f46db00cf82746bddfa0dc7cae43
Zachary <ZacharyZhang1990@gmail.com>
2019-03-21 10:01:59 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <router-view/> -->
+    <Header/>
+    <!-- <PostList/> -->
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+/* default setting */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+ul,
+li {
+  list-style-type: none;
+}
+a {
+  text-decoration: none;
+}
+.clearfix::after {
+  content: "";
+  display: block;
+  clear: both;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="header">
+    <div class="wrapper clearfix">
+      <div class="log">
+        <img src="../assets/cnodejs_light.svg">
+      </div>
+      <div class="seacher"></div>
+      <div class="nav">
+        <ul>
+          <li>
+            <a href="#">首页</a>
+          </li>
+          <li>
+            <a href="#">未读消息</a>
+          </li>
+          <li>
+            <a href="#">新手入门</a>
+          </li>
+          <li>
+            <a href="#">API</a>
+          </li>
+          <li>
+            <a href="#">关于</a>
+          </li>
+          <li>
+            <a href="#">设置</a>
+          </li>
+          <li>
+            <a href="#">退出</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+
+<style scoped>
+.header {
+  height: 50px;
+  background: #444444;
+}
+.wrapper {
+  margin-left: 100px;
+  margin-right: 100px;
+}
+.header .log {
+  float: left;
+  width: 120px;
+  height: 50px;
+  display: flex;
+  border: 1px solid red;
+  margin-right: 20px;
+}
+.header .log img{
+     width: 120px;
+}
+
+
+.header .seacher {
+  float: left;
+  width: 233px;
+  height: 26px;
+  border: 1px solid green;
+}
+.header .nav {
+  float: right;
+  width: 622px;
+  border: 1px solid blue;
+}
+.header ul {
+  display: flex;
+}
+.header ul > li > a {
+  /* color: #ffffff; */
+  color: #cccccc;
+  font-size: 13px;
+  font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma,
+    "Hiragino Sans GB", STHeiti, sans-serif !important;
+}
+</style>
